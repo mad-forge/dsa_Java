bc1933ee77b01e908eaf9ad5bbc13307ac40fd00
mad-forge <shashwat11muz@gmail.com>
2026-04-14 16:52:28 +0530

    5 basic questions

---
+++ src/basic_Questions.java
@@ -0,0 +1,64 @@
+//sum of two numbers
+
+//public class basic_Questions {
+//    public static void main (String[] args){
+//        int a = 10;
+//        int b = 5;
+//        int sum = a + b;
+//        System.out.println("Sum: " + sum);
+//    }
+//}
+
+//area of rectangle
+
+//public class basic_Questions {
+//    public static void main(String[] args){
+//        int length = 10;
+//        int breadth = 20;
+//        int area = length * breadth;
+//        System.out.println("Area of Rectangle is: " + area);
+//    }
+//}
+
+//area of circle
+
+//public class basic_Questions {
+//    public static void main(String[] args){
+//        double radius = 10;
+//        double area = Math.PI * radius * radius;
+//        System.out.println("Area of Circle: "+ area);
+//    }
+//}
+
+//celsius to fahrenheit conversion
+
+//public class basic_Questions {
+//    public static void main(String[] args){
+//        double celsius = 20;
+//        double fahrenheit = (celsius * 9 / 5) + 32;
+//        System.out.println(fahrenheit);
+//    }
+//}
+
+//print bioData
+
+public class basic_Questions {
+    public static void main (String[] args){
+        String name = "Shashwat";
+        int age = 21;
+        String city = "Patna";
+        String collegeCity = "Bhubaneswar";
+        String college = "KIIT University";
+        String course = "B.Tech";
+        int batch = 2025;
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("City: " + city);
+        System.out.println("College City: " + collegeCity);
+        System.out.println("College Name: " + college );
+        System.out.println("Course: "+ course);
+        System.out.println("Batch: " + batch);
+
+    }
+}
