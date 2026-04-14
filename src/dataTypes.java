39264723fad144d92eda83a148895e4db5fce8e3
mad-forge <shashwat11muz@gmail.com>
2026-04-14 15:56:09 +0530

    java, primitive dataTypes

---
+++ src/dataTypes.java
@@ -0,0 +1,88 @@
+//boolean dataType
+
+//public class dataTypes{
+//    public static void main(String[] args){
+//        boolean isJavaFun = true;
+//        boolean isFishTasty = false;
+//        System.out.println("Is Java fun? " + isJavaFun);
+//        System.out.println("Is Fish tasty? " + isFishTasty);
+//    }
+//}
+
+//byte dataType
+
+//public class dataTypes{
+//    public static void main(String[] args){
+//        byte age = 25;
+//        byte temperature = -10;
+//        System.out.println("Age: " + age);
+//        System.out.println("Temperature: " + temperature);
+//    }
+//}
+
+//short dataType
+
+//public  class dataTypes{
+//    public static void main(String[] args){
+//        short students = 1000;
+//        short temp = -200;
+//        System.out.println("Students: " + students);
+//        System.out.println("Temperature: " + temp);
+//    }
+//}
+
+// int dataType
+
+//public class dataTypes{
+//    public static void main(String[] args){
+//        int population = 2000000;
+//        int distance = 150000000;
+//        System.out.println("Population: " + population);
+//        System.out.println("Distance: " + distance);
+//    }
+//}
+
+// long Datatypes
+
+//public class dataTypes {
+//    public static void main(String[] args) {
+//        long worldPopulation = 7800000000L;
+//        long lightYears = 9460730472580800L;
+//        System.out.println("World Population: " + worldPopulation);
+//        System.out.println("Light Years: " + lightYears);
+//    }
+//}
+
+//float dataTypes
+
+//public class dataTypes {
+//    public static void main(String[] args) {
+//        float pi = 3.14f;
+//        float gravity = 9.81f;
+//        System.out.println("Pi: " + pi);
+//        System.out.println("Gravity: " + gravity);
+//    }
+//}
+
+// double dataTypes
+
+//public class dataTypes {
+//    public static void main(String[] args) {
+//        double pi = 3.141592653589793;
+//        double avogadro = 6.02214076e23;
+//        System.out.println("Pi: " + pi);
+//        System.out.println("Avogadro's Number: " + avogadro);
+//    }
+//}
+
+//char dataTypes
+
+//public class dataTypes {
+//    public static void main(String[] args) {
+//        char grade = 'A';
+//        char symbol = '$';
+//        System.out.println("Grade: " + grade);
+//        System.out.println("Symbol: " + symbol);
+//    }
+//}
+
